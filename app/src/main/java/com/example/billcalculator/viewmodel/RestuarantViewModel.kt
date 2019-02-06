9fde1dbb40a73e49b98f4421152df76b05dcc2c7
festus <kiamfestoh@gmail.com>
2019-02-06 16:19:34 +0300

    add data binding to input fields

--- app/src/main/java/com/example/billcalculator/viewmodel/RestuarantViewModel.kt
+++ app/src/main/java/com/example/billcalculator/viewmodel/RestuarantViewModel.kt
@@ -1,26 +1,34 @@
 package com.example.billcalculator.viewmodel
 
 import android.util.Log
+import androidx.databinding.BaseObservable
 import com.example.billcalculator.model.RestuarantTipCalculator
 import com.example.billcalculator.model.TipCalculation
 
 /**
  * Created by Festus Kiambi on 2/6/19.
  */
-class RestuarantViewModel(val restuarantTipCalculator: RestuarantTipCalculator = RestuarantTipCalculator()) {
+class RestuarantViewModel(
+    val restuarantTipCalculator: RestuarantTipCalculator = RestuarantTipCalculator()
+) : BaseObservable() {
 
     var inputCheckAmount = ""
     var inputTipPercentage = ""
     var calculation = TipCalculation()
 
-    fun calculateTip(): TipCalculation{
+    fun calculateTip() {
         val checkAmount = inputCheckAmount.toDoubleOrNull()
-        val tipPercentage = inputTipPercentage .toIntOrNull()
+        val tipPercentage = inputTipPercentage.toIntOrNull()
 
-        if(checkAmount != null && tipPercentage !=null) {
-
+        if (checkAmount != null && tipPercentage != null) {
             calculation = restuarantTipCalculator.calculateTip(checkAmount, tipPercentage)
+            clearinputs()
         }
-        return this.calculation
+    }
+
+    private fun clearinputs() {
+        inputTipPercentage = ""
+        inputCheckAmount = ""
+        notifyChange()
     }
 }
